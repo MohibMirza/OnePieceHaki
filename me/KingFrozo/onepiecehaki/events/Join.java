5b8c3e429e537f4c0bf662bc3683c75320bfdfdc
Mohib Mirza <mohibumirza@gmail.com>
2017-11-27 15:35:19 -0800

    Create Join.java

---
+++ me/KingFrozo/onepiecehaki/events/Join.java
@@ -0,0 +1,24 @@
+package me.KingFrozo.onepiecehaki.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+import me.KingFrozo.onepiecehaki.Main;
+
+public class Stats /*implements CommandExecutor*/ {
+	  
+	  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+	  {
+	    if ((sender instanceof Player))
+	    {
+	      Player p = (Player)sender;
+	      p.sendMessage("Stats are not yet available!");
+	      // addDeath(p.getName());
+	    }
+	    return false;
+	  }
+
+}
